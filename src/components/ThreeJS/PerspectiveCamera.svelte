17f0630253fdd44382777e513be6f38714863480
RonHouben <ron.houben85@gmail.com>
2021-10-31 08:07:21 +0100

    feat: PerspectiveCamera now can be rotated

--- src/components/ThreeJS/PerspectiveCamera.svelte
+++ src/components/ThreeJS/PerspectiveCamera.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { AnimationOptions, PositionOptions, ThreeController } from '../../controllers/three.controller';
+import { AnimationOptions, PositionOptions, RotateOptions, ThreeController } from '../../controllers/three.controller';
 	import { PerspectiveCamera } from 'three'
 	import { perspectiveCamera as camera } from '../../stores/threejs/perspective.camera.store'
 import { scene } from '../../stores/threejs/scene.store';
@@ -11,6 +11,7 @@ import { scene } from '../../stores/threejs/scene.store';
 	export let far: number
 	export let position: PositionOptions
 	export let animate: AnimationOptions = {}
+	export let rotate: RotateOptions = {}
 
 	// set camera store state
 	camera.set(new PerspectiveCamera(fov, aspect, near, far))
@@ -21,6 +22,7 @@ import { scene } from '../../stores/threejs/scene.store';
 
 	// set camera position
 	cameraController.position(position)
+	cameraController.rotate(rotate)
 
 	// animation loop
 	cameraController.animate(animate)
