0c308351295a0aee95192325f965fecfda1fb2bb
RonHouben <ron.houben85@gmail.com>
2021-10-31 08:08:01 +0100

    feat: added AxesHelper and GridHelper to Scene

--- src/components/ThreeJS/Scene.svelte
+++ src/components/ThreeJS/Scene.svelte
@@ -4,23 +4,43 @@
 	import { Scene, WebGLRenderer } from 'three'
 	import { perspectiveCamera } from '../../stores/threejs/perspective.camera.store'
 	import { scene } from '../../stores/threejs/scene.store'
+	import Stats from 'three/examples/jsm/libs/stats.module'
+	import { AxesHelper } from 'three'
+	import { GridHelper } from 'three'
 
 	export let sceneId: string
 	export let height: number = 0
 	export let width: number = 0
 	export let background: Color | Texture
+	export let showStats: boolean = false
+	export let showAxesHelper: boolean = false
+	export let showGridHelper: boolean = false
 
 	// set the scene store value to a new scene
 	scene.set(new Scene())
 
+	const stats = Stats()
+
 	// set the scenes properties
 	$: $scene.background = background
+
+	// set helpers
+	if (showAxesHelper) {
+		const axesHelper = new AxesHelper(5)
+		$scene.add(axesHelper)
+	}
+
+	if (showGridHelper) {
+		const gridHelper = new GridHelper()
+		$scene.add(gridHelper)
+	}
 
 	// create a new renderen
 	const renderer = new WebGLRenderer()
 
 	// resize the renderer if the width || height change
 	$: renderer.setSize(width, height)
+	renderer.shadowMap.enabled = true
 
 	onMount(() => {
 		const sceneElement = document.getElementById(sceneId)
@@ -30,6 +50,10 @@
 		} else {
 			// add renderer to the DOM
 			sceneElement.appendChild(renderer.domElement)
+
+			if (showStats) {
+				sceneElement.appendChild(stats.dom)
+			}
 
 			// set height && width based on the parentsElement
 			// if the width || height is not given as a prop to the Scene component
@@ -58,11 +82,13 @@
 		requestAnimationFrame(animate)
 		// TODO: use orbit controls to move the camera
 		renderer.render($scene, $perspectiveCamera)
+
+		stats.update()
 	}
 </script>
 
 <div id={sceneId}>
-	<slot name="light" />
+	<slot name="lights" />
 	<slot name="camera">
 		<!-- TODO: Create errorMessage component -->
 		<p>
@@ -70,5 +96,5 @@
 			`slot="camera"` to the Camera component?
 		</p>
 	</slot>
-	<slot name="mesh" />
+	<slot name="meshes" />
 </div>
